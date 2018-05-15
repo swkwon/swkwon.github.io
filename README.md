a775bd1ca98f79414b1e04233094ff111cc681df
Sangwook, Kwon <swkwon04@gmail.com>
2018-05-16 00:39:35 +0900

    change blog

--- README.md
+++
@@ -1,1 +0,0 @@
-# swkwon.github.io
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Markdown 정리",
-        "excerpt":"헤더 html의 h1 또는 h2와 같은 헤더 태그는 #으로 표시할 수 있다. # 이것은 h1## 이것은 h2### 이것은 h3#### 이것은 h4##### 이것은 h5###### 이것은 h6code snippet 코드를 넣을 때는 아래와 같이 입력한다. {% highlight javascript %}/* this is javascript comment */var raw = [\"r\", \"a\", \"w\"];{% endhighlight %}이탤릭, 볼드, 선긋기...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/markdown-%EC%A0%95%EB%A6%AC/",
-        "teaser":null}]
